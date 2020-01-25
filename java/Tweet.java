79b4bcb74e08cd9d63be6f1c00ca52a83c7e8868
Jose Sotolongo <joseesotolongo@gmail.com>
2020-01-24 20:06:27 -0500

    Initializing WeightCalculator

--- java/Tweet.java
+++ java/Tweet.java
@@ -1,5 +1,3 @@
-package bigdata;
-
 import java.util.ArrayList;
 
 public class Tweet 
---
+++ java/WeightCalculator.java
@@ -0,0 +1,37 @@
+// total = sum of first 7 in the array
+// 8th is not counted in the count
+// divide by each one by the total including the 8th 
+// update the values of each indicies of the array with the new calculated weight
+// if the calculated weight of the categories from 1-7 threshold is less then 1/5 and catorgory 8 is any number rn
+// Individual total depending on how many times that word occured.
+// Keep track of the string 
+// Ignore all zeros
+
+
+// Write to a file
+
+import java.ClassifyTweet.TrieNode;
+
+public class WeightCalculator
+{
+     TrieNode temp;
+
+     /**
+      * Takes in already filled trie and passes it through a traversal function
+      * that is going to add the existing children to a stack.
+      * @param root
+      */
+     public WeightCalculator(TrieNode root)
+     {
+          traverse(root);
+     }
+
+     /**
+      * Traverse through the Trie and add an existing children to
+      * @param root
+      */
+     private void traverse(TrieNode root)
+     {
+
+     }
+}
