83eee1adcf65a17b999262ea37ec59262d50ed4f
mangose10 <>
2020-01-31 13:53:25 -0500

    geoLocate added

---
+++ geoLocate/api/controllers/view-by-category.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View by category',
+
+
+  description: 'Display "By category" page.',
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/by-category'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    
+    // Respond with view.
+    return {};
+
+  }
+
+
+};
---
+++ geoLocate/api/models/Location.js
@@ -0,0 +1,42 @@
+/**
+ * Location.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    longitude : {
+        type: "double",
+    },
+
+    latitude : {
+        type: "double",
+    },
+
+    category : {
+        type: "int",
+    },
+
+    tweet : {
+        type: "string",
+    },
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
